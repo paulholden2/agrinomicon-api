49d5143126efd6c56fe9f91008221c09e0129857
Paul Holden <paul@paulholden.dev>
2023-06-03 07:13:57 -0700

    Add GeoJSON tests with corrected geometry fixture

---
+++ test/agrinomicon_web/controllers/feature_geojson_test.exs
@@ -0,0 +1,21 @@
+defmodule AgrinomiconWeb.FeatureGEOJSONTest do
+  use AgrinomiconWeb.ConnCase, async: true
+
+  import Agrinomicon.GisFixtures
+
+  test "renders single feature" do
+    feature = feature_fixture()
+    assert AgrinomiconWeb.FeatureGEOJSON.show(%{feature: feature})
+           |> Jason.encode!()
+           |> Jason.decode!()
+           |> Geo.JSON.decode!()
+  end
+
+  test "renders list of feature" do
+    feature = feature_fixture()
+    assert AgrinomiconWeb.FeatureGEOJSON.index(%{features: [feature, feature]})
+           |> Jason.encode!()
+           |> Jason.decode!()
+           |> Geo.JSON.decode!()
+  end
+end
